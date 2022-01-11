fb24ab0bbfeadb298eb3d57cac251c02fec4cbad
Oliver Foster <oliver.foster@kineo.com>
2022-01-11 13:01:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# bower-fix-2610
+
+Fix for bower issue 2610, github no longer supports git:// protocol
+Only when using bower as a library, will not work for command line
+
+## Use
+1. Install into the project:
+`$ npm install bower-fix-2610`
+
+2. Require or import:
+`require('bower-fix-2610')`
+`import 'bower-fix-2610'`
---
+++ index.js
@@ -0,0 +1,24 @@
+const Mod = require('module')
+// Monkey patch global require function
+const o = Mod.prototype.require
+Mod.prototype.require = function () {
+  const module = o.apply(this, arguments)
+  const name = arguments[0]
+  // Check for require('child_process')
+  if (name !== 'child_process') return module
+  // Monkey patch spawn function
+  const p = module.spawn
+  module.spawn = function () {
+    const cmd = arguments[0]
+    const args = arguments[1]
+    // Check that arguments begin ['git', ['ls-remote', ...]]
+    if (typeof cmd !== 'string' || !/git/.test(cmd) || !Array.isArray(args) || args[0] !== 'ls-remote') return p.apply(this, arguments)
+    arguments[1] = args.map(function (arg) {
+      if (typeof arg !== 'string') return arg
+      // Replace git://github.com with https://github.com
+      return arg.replace(/git:\/\/github.com/, 'https://github.com')
+    })
+    return p.apply(this, arguments)
+  }
+  return module
+}
